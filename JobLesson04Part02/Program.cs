44c7036fc094716e9731db59440ed68867703e81
Safroniv <safroniv@mail.ru>
2022-02-08 21:27:14 +0300

    Урок 4 Задание 2

--- JobLesson04Part02/Program.cs
+++ JobLesson04Part02/Program.cs
@@ -2,3 +2,19 @@
 //Написать программу, принимающую на вход строку — набор чисел, 
 //разделенных пробелом, и возвращающую число — сумму всех чисел в строке. 
 //Ввести данные с клавиатуры и вывести результат на экран.
+
+//получение строки чисел с пробелами:
+string setNumbers = Convert.ToString(Console.ReadLine());
+//разделяем на подстроки каждый элемент строки и делаем массив:
+string[] subs = setNumbers.Split();
+//преобразуем масив строк в массив int-ов:
+int[] summNumbers = subs.Select(x => int.Parse(x)).ToArray();
+
+//Суммируем все элементы массива:
+int summa = 0;
+for (int i = 0; i < summNumbers.Length; i++)
+{
+    summa = summa + summNumbers[i];      
+}
+//Выводим значение:
+Console.WriteLine(summa);  
