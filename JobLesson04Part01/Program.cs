fb04bdc436a64ca101931f4418abcb351d775fcb
Safroniv <safroniv@mail.ru>
2022-02-08 20:00:07 +0300

    Урок 4 Задание 1

--- JobLesson04Part01/Program.cs
+++ JobLesson04Part01/Program.cs
@@ -3,3 +3,16 @@
 //принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО.
 //Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.
 
+//получение исходных данных для метода:
+{
+    Console.WriteLine(GetFullName("Иван", "Иванов", "Иванович"));
+    Console.WriteLine(GetFullName("Пётр", "Петров", "Петрович"));
+    Console.WriteLine(GetFullName("Сидор", "Сидоров", "Сидорович"));
+    Console.WriteLine(GetFullName("Сергей", "Сергеев", "Сергеевич"));
+}
+//Возвращение объёдинённой строки с ФИО:
+string GetFullName(string firstName, string lastName, string patronymic)
+{
+    string FCs = lastName + " " + firstName + " " + patronymic;
+    return FCs;
+}
