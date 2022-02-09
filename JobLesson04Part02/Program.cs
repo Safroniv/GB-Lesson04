00ea8e92aa70ff139dfae5414ea0ac2d03b16ab3
Safroniv <safroniv@mail.ru>
2022-02-09 21:42:42 +0300

    Промежуточный вариант решения задания 2 Урока 4

--- JobLesson04Part02/Program.cs
+++ JobLesson04Part02/Program.cs
@@ -3,18 +3,44 @@
 //разделенных пробелом, и возвращающую число — сумму всех чисел в строке. 
 //Ввести данные с клавиатуры и вывести результат на экран.
 
-//получение строки чисел с пробелами:
 string setNumbers = Convert.ToString(Console.ReadLine());
-//разделяем на подстроки каждый элемент строки и делаем массив:
-string[] subs = setNumbers.Split();
-//преобразуем масив строк в массив int-ов:
-int[] summNumbers = subs.Select(x => int.Parse(x)).ToArray();
-
-//Суммируем все элементы массива:
-int summa = 0;
-for (int i = 0; i < summNumbers.Length; i++)
+char[] chars = setNumbers.ToCharArray();
+string summChars = "";
+for (int i =0; i<chars.GetLength(0); i++)
 {
-    summa = summa + summNumbers[i];
+    if (chars[i] !=' ')
+    {
+        summChars += chars[i];
+    }
 }
-//Выводим значение:
-Console.WriteLine(summa);
+Console.WriteLine(summChars);
+//int[] summNumbers = { 12, 312, 4324, 432, 42 };
+//int summa = 0;
+//for (int i = 0; i < summNumbers.Length; i++)
+//{
+//    summa = summa + summNumbers[i];
+//}
+////Выводим значение:
+//Console.WriteLine(summa);
+
+//Алтернативный вариант #1
+////получение строки чисел с пробелами:
+//string setNumbers = Convert.ToString(Console.ReadLine());
+////разделяем на подстроки каждый элемент строки и делаем массив:
+//string[] subs = setNumbers.Split();
+////преобразуем масив строк в массив int-ов:
+//int[] summNumbers = subs.Select(x => int.Parse(x)).ToArray();
+////Суммируем все элементы массива:
+//int summa = 0;
+//for (int i = 0; i < summNumbers.Length; i++)
+//{
+//    summa = summa + summNumbers[i];
+//}
+////Выводим значение:
+//Console.WriteLine(summa);
+
+//Альтернативный варинат #2
+//var sum = Console.ReadLine().Split().Select(int.Parse).Sum();
+//Console.WriteLine(sum);
+//Альтернативный варинат #3
+//Console.WriteLine(Console.ReadLine().Split().Select(int.Parse).Sum());
