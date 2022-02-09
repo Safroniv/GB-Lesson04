e0ef7a08e94c798d76da2883323c2736efa0789a
Safroniv <safroniv@mail.ru>
2022-02-10 02:46:36 +0300

    Урок 3 Задание 3

--- JobLesson04Part03/Program.cs
+++ JobLesson04Part03/Program.cs
@@ -8,5 +8,66 @@
 
 Console.WriteLine("Здравствуйте, подскажите номер текущего месяца?");
 int numMonth = Int32.Parse(Console.ReadLine());
+SeasonsYear seasons = SeasonIndentification(numMonth);
+string seasonOfYear = NameMonth(seasons);
+Console.WriteLine(seasonOfYear);
 
-Console.WriteLine(numMonth);
+
+static SeasonsYear SeasonIndentification(int numMonth)
+{
+
+    switch (numMonth)
+    {
+        case 1:
+        case 2:
+        case 12:
+            return SeasonsYear.Winter;
+        case 3:
+        case 4:
+        case 5:
+            return SeasonsYear.Spring;
+        case 6:
+        case 7:
+        case 8:
+            return SeasonsYear.Summer;
+        case 9:
+        case 10:
+        case 11:
+            return SeasonsYear.Autumn;
+            break;
+    }
+    return SeasonsYear.Other;
+}
+   static string NameMonth(SeasonsYear seasons)
+{
+    switch (seasons)
+    {
+        case SeasonsYear.Winter:
+            {
+                return "Зима";
+            }
+        case SeasonsYear.Spring:
+            {
+                return "Весна";
+            }
+        case SeasonsYear.Summer:
+            {
+                return "Лето";
+            }
+        case SeasonsYear.Autumn:
+            {
+                return "Лето";
+            }
+        default:
+            return "Ошибка: введите число от 1 до 12";
+    }
+}
+enum SeasonsYear
+{
+    Winter = 1,
+    Spring,
+    Summer,
+    Autumn,
+    Other
+
+}
