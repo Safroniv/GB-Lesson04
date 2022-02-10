12e64a7b173aac9b05df8eebb3d897a9f86cc107
Safroniv <safroniv@mail.ru>
2022-02-10 03:48:59 +0300

    Урок 4 Задание 4

--- JobLesson04Part04/Program.cs
+++ JobLesson04Part04/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Урок 4 Задание 4
+//Написать программу, вычисляющую число Фибоначчи для заданного значения рекурсивным способом. 
+int number = 20;
+Console.WriteLine(GetFibonacci(number));
+
+static int GetFibonacci(int number)
+{
+    if (number == 0||number==1)
+    {
+        return number;
+    }
+    return GetFibonacci(number - 1) + GetFibonacci(number  - 2);
+}
